34e78cf710557b454357c347ddb9eba6fd469d13
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-21 22:14:12 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,17 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
+pp nds
+# Change the code below to pretty print the nds with pp
   nil
 end
 
 def print_first_directors_movie_titles
+  element_index=0
+  while element_index < directors_database[0][:movies].length
+   print directors_database[0][:movies][element_index][:title]
+    element_index +=1
+  end
 end
+
+
